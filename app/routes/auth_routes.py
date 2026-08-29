2e3834dc1a7a7b412150aee5d680e97afcd7a4e1
Sarang <your-email@example.com>
2026-08-29 09:57:10 +0530

    database configuration

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,2 +1,4 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, checjuishfeh
+from app.models.db import mysql
+
